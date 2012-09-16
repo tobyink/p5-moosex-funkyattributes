c68cb68faeb43c9853cc313a896f5ce745fe4316
Toby Inkster <mail@tobyinkster.co.uk>
2012-09-16 19:28:54 +0100

    custom_weaken for InsideOut

--- lib/MooseX/FunkyAttributes/Role/Attribute/InsideOut.pm
+++ lib/MooseX/FunkyAttributes/Role/Attribute/InsideOut.pm
@@ -27,6 +27,7 @@ before _process_options => sub
 	$options->{custom_set}           = sub { $h{ $_[1] } = $_[2] };
 	$options->{custom_has}           = sub { exists $h{ $_[1] } };
 	$options->{custom_clear}         = sub { delete $h{ $_[1] } };
+	$options->{custom_weaken}        = sub { Scalar::Util::weaken( $h{ $_[1] } ) };
 	$options->{custom_inline_get}    = sub { my ($self, $inst) = @_; qq(\$MooseX::FunkyAttributes::Role::Attribute::InsideOut::_HASHES[$hashcount]{$inst};) };
 	$options->{custom_inline_set}    = sub { my ($self, $inst, $val) = @_; qq(\$MooseX::FunkyAttributes::Role::Attribute::InsideOut::_HASHES[$hashcount]{$inst} = $val;) };
 	$options->{custom_inline_weaken} = sub { my ($self, $inst) = @_; qq(Scalar::Util::weaken \$MooseX::FunkyAttributes::Role::Attribute::InsideOut::_HASHES[$hashcount]{$inst};) };
