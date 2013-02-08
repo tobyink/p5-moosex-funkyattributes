d364728279209cfc308652c57690913bf66cdb72
Toby Inkster <mail@tobyinkster.co.uk>
2013-02-08 17:56:31 +0000

    clean example

--- examples/inside-out.pl
+++ examples/inside-out.pl
@@ -1,22 +1,20 @@
-package Person;
+use v5.14;
 
-use Moose;
-use MooseX::FunkyAttributes;
+package Person {
+	use Moose;
+	use MooseX::FunkyAttributes;
 
-has name => (
-	traits => [ InsideOutAttribute ],
-	is     => 'ro',
-	isa    => 'Str',
-);
+	has name => (
+		traits => [ InsideOutAttribute ],
+		is     => 'ro',
+		isa    => 'Str',
+	);
 
-has age => (
-	is     => 'ro',
-	isa    => 'Num',
-);
-
-package main;
-
-use feature 'say';
+	has age => (
+		is     => 'ro',
+		isa    => 'Num',
+	);
+}
 
 my $bob = Person->new(name => 'Bob', age => 32);
 say $bob->name;     # Bob
