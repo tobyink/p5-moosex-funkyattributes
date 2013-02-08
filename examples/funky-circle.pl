9e0bb765ab5b611d779030a4bc6a6f903e1e34e4
Toby Inkster <mail@tobyinkster.co.uk>
2013-02-08 18:24:56 +0000

    add pod to examples too; update changelog

--- examples/funky-circle.pl
+++ examples/funky-circle.pl
@@ -1,3 +1,21 @@
+=head1 PURPOSE
+
+Example showing the use of L<MooseX::FunkyAttributes> and
+L<MooseX::CustomInitArgs> together.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2013 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
 use v5.14;
 
 package Circle {
--- examples/inside-out.pl
+++ examples/inside-out.pl
@@ -1,3 +1,20 @@
+=head1 PURPOSE
+
+Basic example showing how to store one attribute inside out.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2012-2013 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
 use v5.14;
 
 package Person {
