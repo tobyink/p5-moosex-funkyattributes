68f6985558fdc6a1395320d46080824932d852e7
Toby Inkster <mail@tobyinkster.co.uk>
2014-03-26 15:21:37 +0000

    bump version; changelog

--- lib/MooseX/FunkyAttributes/Role/Attribute/InsideOut.pm
+++ lib/MooseX/FunkyAttributes/Role/Attribute/InsideOut.pm
@@ -6,7 +6,7 @@ use warnings;
 
 BEGIN {
 	$MooseX::FunkyAttributes::Role::Attribute::InsideOut::AUTHORITY = 'cpan:TOBYINK';
-	$MooseX::FunkyAttributes::Role::Attribute::InsideOut::VERSION   = '0.002';
+	$MooseX::FunkyAttributes::Role::Attribute::InsideOut::VERSION   = '0.003';
 }
 
 use Hash::FieldHash ();
@@ -105,7 +105,7 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2012-2013 by Toby Inkster.
+This software is copyright (c) 2012-2014 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
